e90c92e030858b78197cb23c978b18b70c182613
tangpeng918@126.com <tangpeng918@126.com@f4f52416-e61e-11de-a3ff-fbe938f12fc4>
2010-02-03 16:38:51 +0000

    状态机增加Lua解析代码，未完成！

---
+++ trunk/GServerEngine/GameAi/GameAi/Script/BLuaScript.h
@@ -0,0 +1,42 @@
+/*
+* 
+*
+*/	
+#pragma  once
+
+namespace  tp_script
+{
+
+	enum  eLuaError
+	{
+		 LUA_CREATE_ERROR			= 1,
+		 LUA_SCRIPT_LEN_ERROR		= 2,
+		 LUA_SCRIPT_COMPILE_ERROR	= 3,
+		 LUA_SCRIPT_EXECUTE_ERROR	= 4,
+		 LUA_SCRIPT_NOT_NUMBER_ERROR=10,
+		 LUA_SCRIPT_NOT_STRING_ERROR=11,
+		 LUA_SCRIPT_NOT_TABLE_ERROR =12,
+		 LUA_SCRIPT_STATES_IS_NULL  =20
+	};
+	class   CBaseScript
+	{
+	public:
+		CBaseScript(); 
+		virtual ~CBaseScript();
+
+		virtual bool  Init()	{  return  true  ; }
+
+		virtual void  Exit()	{  return  true  ; }
+
+		virtual bool  Load(const char* FileName )	{  return  true  ; }
+
+		virtual bool  Compile(const char* FileName)	{  return  true  ; }
+
+		virtual bool  Execute()						{  return  true  ; }
+
+		virtual bool  CallFunction(char* cFunName , int nResults , char* cFormat, ... );
+
+		virtual bool  RegFunction(char* cFunName , void * Func )
+	};
+
+}
---
+++ trunk/GServerEngine/GameAi/GameAi/Script/CLuaScript.cpp
@@ -0,0 +1,517 @@
+#include "stdafx.h"
+#include "CLuaScript.h"
+
+
+namespace tp_script
+{
+
+	/// 构造函数
+	CLuaScript::CLuaScript()
+	{
+		m_LuaState  = lua_open( 100 );
+
+		if ( m_LuaState == NULL )
+		{
+			ScriptError( LUA_CREATE_ERROR );
+			m_IsRuning = false;
+			return ;
+		}
+
+		m_IsRuning = true;
+		m_szScriptName[0] = '\0';
+	}
+
+// 	/// 
+// 	CLuaScript::CLuaScript(int StackSize = 0)
+// 	{
+// 		m_LuaState				= Lua_Create(StackSize);
+// 
+// 		if (m_LuaState == NULL )
+// 		{
+// 			ScriptError(LUA_CREATE_ERROR);
+// 			m_IsRuning = false;
+// 			return ;
+// 		}
+// 		m_IsRuning				= true;
+// 		m_szScriptName[0]		= '\0';
+// 	}
+
+	/// 析构函数
+	CLuaScript::~CLuaScript()
+	{
+		Exit();
+	}
+
+	bool CLuaScript::LoadBuffer(unsigned char *pBuffer, size_t dwLen)
+	{
+		if ( dwlen < 0 )
+		{
+			ScriptError( LUA_SCRIPT_LEN_ERROR );
+			return false;
+		}
+
+		if ( lua_compileBuffer(m_LuaState , (char*) pBuffer , dwlen ) != 0 )
+		{
+			ScriptError( LUA_SCRIPT_COMPILE_ERROR );
+			return false;
+		}
+		return true;
+	}
+
+	bool  CLuaScript::Load(const char *FileName)
+	{
+		try
+		{
+			/// read filename buffer
+			/// pCon is filename context
+			char *pCon = NULL ;
+			size_t size= 0 ;
+			if ( !LoadBuffer( pCon , size /**/) )
+			{
+				ScriptError( LUA_SCRIPT_COMPILE_ERROR );
+				return false;
+			}
+		}
+		catch( ... )
+		{
+			return false;
+		}
+
+		if ( !ExecuteCode() )
+			return false;
+		return true;
+	}
+
+	/// 执行
+	bool CLuaScript::Execute()
+	{
+		if( m_IsRuning && m_LuaState )
+			return CallFunction("main", 0 , "" );
+		
+		return false;
+	}
+
+	/// 执行
+	bool CLuaScript::ExecuteCode()
+	{
+		if ( !( m_IsRuning && m_LuaState ))
+		{
+			ScriptError( LUA_SCRIPT_EXECUTE_ERROR );
+			return false;
+		}
+
+		int  state ;
+		if ( state = Lua_Execute(m_LuaState) != 0 )
+		{
+			ScriptError( LUA_SCRIPT_EXECUTE_ERROR , state );
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * 函数:	CallFunction
+	 * 功能:	调用Lua脚本内的函数
+	 * 参数:	char* cFuncName
+	 * 参数:	int   nResults
+	 * 参数:	char* cFormat  调用时所传参数的类型 
+	 *			n:数字型(double) d:整形(int) s:字符串型 f:C函数型  
+	            n:Nil v:Value p:Point v形为Lua支持的
+	            参数为整形的数index，指明将index所指堆栈的变量作为该函数的调用参数。	
+
+	 *	注意：由于该函数有不定参数…,对于数字，系统并不确定数是以double还是以int
+	 *  存在，两种保存形式是不同的。因此需要注意当传入的数是整形时，格式符应用d
+	 *  而不能用n,或者强行改变为double形。否则会出现计算的错误。  
+	 */
+
+	bool  CLuaScript::CallFunction(const char* cFuncName , int nResults ,
+									char* cFormat, va_list vlist)
+	{
+
+		double nNumber ;
+		char*  cString =  NULL;
+		char*  pPoint  =  NULL;
+		
+		lua_CFunction   CFunc;
+		
+		int    i = 0 , nArgNum = 0 , nIndex = 0 , nRetcode = 0;
+
+		if ( ! (m_IsRuning && m_LuaState) )
+		{
+			ScriptError( LUA_SCRIPT_STATES_IS_NULL  );
+			return false;
+		}
+
+		Lua_GetGlobal( m_LuaState , cFuncName );
+
+		while ( cFormat[i] != '\0' )
+		{
+			switch ( cFormat[i] )
+			{
+				/// double 
+			case 'n':
+				{
+					nNumber = va_arg( vlist ,double);
+					Lua_pushNumber( m_LuaState , nNumber );
+					nArgnum ++ ;
+				}
+				break;
+
+				///  int 
+			case 'd':
+				{
+					nNumber = (double)(va_arg(vlist, int));
+					Lua_PushNumber(m_LuaState, (double) nNumber);
+					nArgnum ++;
+				}
+				break;
+
+				///  string
+			case 's':
+				{
+					cString = va_arg(vlist, char *);
+					Lua_PushString(m_LuaState, cString);
+					nArgnum ++;							
+				}
+				break;
+
+				/// Null
+			case 'N':
+				{
+					Lua_PushNil(m_LuaState);
+					nArgnum ++;
+				}
+				break;
+
+				/// call function
+			case 'f':
+				{
+					CFunc = va_arg(vlist, Lua_CFunction);
+					Lua_PushCFunction(m_LuaState, CFunc) ;
+					nArgnum ++;
+				}
+				break;
+
+			case 'v'://输入的是堆栈中Index为nIndex的数据类型
+				{
+					nNumber = va_arg(vlist, int);
+					int nIndex1 = (int) nNumber;
+					Lua_PushValue(m_LuaState, nIndex1);
+					nArgnum ++;
+				}
+				break;
+			case 't'://输入为一Table类型
+				{
+
+				}
+				break;
+
+			case 'p':
+				{
+					pPoint = va_arg(vlist, void *);
+
+					Lua_PushUserTag(m_LuaState, pPoint,m_UserTag);
+					nArgnum ++;
+				}
+				break;
+			}
+			i++;	
+		}
+
+		nRetcode = Lua_Call( m_LuaState , nArgnum , nResults );
+		if ( nRetcode != 0 )
+		{
+			ScriptError( LUA_SCRIPT_EXECUTE_ERROR , state );
+			return false;
+		}
+		return true;
+	}
+
+
+	bool CLuaScript::CallFunction(const char* cFuncName, int nResults, char* cFormat, ...)
+	{
+		bool bResult  = false;
+		va_list vlist;
+		va_start(vlist, cFormat);
+		bResult = CallFunction(cFuncName, nResults, cFormat, vlist);
+		va_end(vlist);
+		return bResult;
+	}
+
+	// 函数:	CLuaScript::GetValuesFromStack
+	// 功能:	从堆栈中获得变量
+	bool CLuaScript::GetValuesFromStack(char * cFormat, ...)	
+	{
+		va_list vlist;
+		double* pNumber = NULL;
+		const char **   pString ;
+		int * pInt = NULL;
+		int i = 0;
+		int nTopIndex = 0;
+		int nIndex = 0;
+		int nValueNum = 0;//cFormat的字符长度，表示需要取的参数数量
+
+		if (! m_LuaState)
+			return false;
+
+		nTopIndex = Lua_GetTopIndex(m_LuaState);	
+		nValueNum = strlen(cFormat);
+
+		if (nTopIndex == 0 || nValueNum == 0)//当堆栈中无数据或不取参数是返回false
+			return false;
+
+		if (nTopIndex < nValueNum)
+			return false;
+
+		nIndex = nTopIndex - nValueNum +1;
+
+		{
+			va_start(vlist, cFormat);     
+
+			while (cFormat[i] != '\0')
+			{
+
+				switch(cFormat[i])
+				{
+				case 'n'://返回值为数值形,Number,此时Lua只传递double形的值
+					{
+						pNumber = va_arg(vlist, double *);
+
+						if (pNumber == NULL)
+							return false;
+
+						if (Lua_IsNumber(m_LuaState, nIndex ))
+						{
+							* pNumber = Lua_ValueToNumber(m_LuaState, nIndex ++ );
+
+						}
+						else
+						{
+							ScriptError(LUA_SCRIPT_NOT_NUMBER_ERROR);
+							return false;
+						}
+
+
+					}
+					break;
+				case 'd':
+					{
+						pInt = va_arg(vlist, int *);
+						if (pInt == NULL)
+							return false;
+						if ( Lua_IsNumber(m_LuaState, nIndex))
+						{
+							* pInt = (int ) Lua_ValueToNumber(m_LuaState, nIndex ++);
+						}
+						else
+						{
+							ScriptError(LUA_SCRIPT_NOT_NUMBER_ERROR);
+							return false;
+						}
+
+					}
+					break;
+				case 's'://字符串形
+					{
+						pString = va_arg(vlist, const char **);
+
+						if (pString == NULL)
+							return false;
+
+						if (Lua_IsString(m_LuaState, nIndex))
+						{
+							(*pString) = (const char *)Lua_ValueToString(m_LuaState, nIndex++);
+
+						}
+						else
+						{
+							ScriptError(LUA_SCRIPT_NOT_STRING_ERROR);
+							return false;
+						}
+					}
+					break;
+
+				}
+
+
+				i ++;	
+			}
+			va_end(vlist);     		/* Reset variable arguments.      */
+
+		}
+		return	true;
+	}
+
+	/// 初始化脚本对象，注册系统标准函数库
+	bool CLuaScript::Init()
+	{
+		if (! m_LuaState)
+		{
+			m_LuaState				= Lua_Create(0);
+
+			if (m_LuaState == NULL)
+			{
+				ScriptError(LUA_CREATE_ERROR);
+				m_IsRuning			= false;
+				return false;
+			}
+
+			m_IsRuning				= true;
+			m_szScriptName[0]		= '\0';
+			m_UserTag = lua_newtag(m_LuaState)	;
+		}
+
+		RegisterStandardFunctions();
+		return	true;
+	}
+
+	//---------------------------------------------------------------------------
+	// 函数:	CLuaScript::RegisterFunction
+	// 功能:	注册某内部C函数至脚本中
+	// 参数:	char* FuncName  在脚本中使用的函数名
+	// 参数:	void* Func    实际相应的C函数指针
+	// 参数:	int Args = 0 //与KScript接口相容，无用
+	// 参数:	int Flag = 0 //与KScript接口相容, 无用
+	//---------------------------------------------------------------------------
+	bool CLuaScript::RegisterFunction(char* FuncName , void* Func)
+	{
+		if (! m_LuaState)
+			return false;
+		Lua_Register(m_LuaState, FuncName, (Lua_CFunction)Func);
+		return true;
+	}
+
+	bool CLuaScript::Compile(char *)
+	{
+		return true;
+	}
+
+	// 功能:	批量注册Lua的内部C函数，各个函数的信息保存在TLua_Funcs的数据中
+	// 参数:	TLua_Funcs *Funcs 数组的指针
+	// 参数:	int n 函数数量。可以为零，由系统计算得到。
+	bool CLuaScript::RegisterFunctions(TLua_Funcs Funcs[], int n)
+	{
+		if (! m_LuaState)	return false;
+		if (n == 0)	n = sizeof(Funcs) / sizeof(Funcs[0]);
+		for (int i = 0; i < n; i ++)	Lua_Register(m_LuaState, Funcs[i].name, Funcs[i].func);
+		return true;
+	}
+
+	/// 注册Lua系统标准的函数库
+	void CLuaScript::RegisterStandardFunctions()
+	{
+		if (! m_LuaState)		return ;
+		Lua_OpenBaseLib(m_LuaState);//Lua基本库
+		Lua_OpenIOLib(m_LuaState);//输入输出库
+		Lua_OpenStrLib(m_LuaState);//字符串处理库
+		Lua_OpenMathLib(m_LuaState);//数值运算库
+		//Lua_OpenDBLib(m_LuaState);//调试库
+		return;	
+	}
+
+
+	//---------------------------------------------------------------------------
+	// 函数:	CLuaScript::ReleaseScript
+	// 功能:	释放该脚本资源。
+	// 返回:	bool 
+	//---------------------------------------------------------------------------
+	void CLuaScript::Exit()
+	{
+
+		if (! m_LuaState)		return ;
+		Lua_Release(m_LuaState);
+		m_LuaState = NULL;
+		m_IsRuning = false;
+
+	}
+	void CLuaScript::ScriptError(int Error)
+	{
+		char lszErrMsg[200];
+		sprintf(lszErrMsg, "ScriptError %d. (%s) \n", Error, m_szScriptName);
+		lua_outerrmsg(lszErrMsg);
+		return;
+	}
+
+	void CLuaScript::ScriptError(int Error1 ,int Error2)
+	{
+		char lszErrMsg[200];
+		sprintf(lszErrMsg, "ScriptError %d:[%d] (%s) \n", Error1, Error2, m_szScriptName);
+		lua_outerrmsg(lszErrMsg);
+		return;
+	}
+
+	//---------------------------------------------------------------------------
+	// 函数:	CLuaScript::SafeCallBegin
+	// SafeCallBegin与SafeCallEnd两函数应搭配使用，以防止在调用Lua的外部函数之后，
+	//有多余数据在堆栈中未被清除。达到调用前与调用后堆栈的占用大小不变。
+	//上述情况只需用在调用外部函数时，内部函数不需如此处理。
+	//																	Romandou
+	//---------------------------------------------------------------------------
+	void CLuaScript::SafeCallBegin(int * pIndex)
+	{
+		if (! m_LuaState)		return ;
+		Lua_SafeBegin(m_LuaState, pIndex);
+	}
+
+	void CLuaScript::SafeCallEnd(int nIndex)
+	{
+		if (! m_LuaState)	return;
+		Lua_SafeEnd(m_LuaState, nIndex);
+	}
+
+	bool CLuaScript::Stop(void)
+	{
+		if (! m_IsRuning)		return true;
+		if (! m_LuaState)		return false;
+		m_IsRuning =  false;
+		return true;
+	}
+
+	// 函数:	CLuaScript::ResumeScript
+	// 功能:	恢复已中止的脚本
+	bool CLuaScript::Resume(void)
+	{
+		if ((! m_IsRuning) && (m_LuaState))
+		{
+			m_IsRuning = true;
+			return true;
+		}
+		return false;
+	}
+
+
+	// 功能:	建立一个Lua的Table，在调用该函数并设置Table各个成员之后，必须调用
+	//			SetGlobalName()来给这个Table指定一个名字。
+	// 返回:	size_t 
+	size_t CLuaScript::CreateTable()
+	{
+		int nIndex = 0;
+
+		nIndex = Lua_GetTopIndex(m_LuaState) ;
+		Lua_NewTable(m_LuaState);
+		if (Lua_GetTopIndex(m_LuaState) != ++nIndex ) 
+			return -1;
+
+		return nIndex;
+	}
+
+	void CLuaScript::SetGlobalName(char* szName)
+	{
+		if (!szName) return ;
+		Lua_SetGlobal(m_LuaState, szName);
+	}
+
+	size_t CLuaScript::ModifyTable(char* szTableName) 
+	{
+		if (! szTableName[0])		return -1;
+
+		int nIndex = Lua_GetTopIndex(m_LuaState);
+
+		Lua_GetGlobal(m_LuaState, szTableName);
+
+		if (Lua_GetTopIndex(m_LuaState) != ++nIndex)		return -1;
+
+		return nIndex;
+	}
+
+}
